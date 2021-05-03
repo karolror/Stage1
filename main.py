98f97ec4f895abe0287543ea6e107f37385c8c4f
Karol <fhkarol@poczta.fm>
2021-05-03 16:16:14 +0200

    Add model selection

--- main.py
+++ main.py
@@ -30,18 +30,24 @@ class MainApplication(tk.Frame):
         self.generate_button.place(x=30, y=450)
         self.load_button = tk.Button(text="Load File", command=self.openfile)
         self.load_button.place(x=713, y=220)
+        self.select_button = tk.Button(text="Select model:", command=lambda: self.select_model(self.get_value_listbox2()))
+        self.select_button.place(x=500, y=220)
+        '#Textboxes'
+        self.textbox = tk.Text(height=1, width=10)
+        self.textbox.place(x=600, y=223)
         '#DB'
         self.client = pymongo.MongoClient(
             "mongodb+srv://db_user1:useruser123@cluster0.qqmqr.mongodb.net/sample_training?retryWrites=true&w=majority")
         '#Options'
         self.loaded_dir = None
+        self.loaded_id = None
 
     def get_value_listbox1(self):
         keykey = self.listbox1.get(tk.ACTIVE)
         return keykey
 
     def get_value_listbox2(self):
-        keykey2 = self.listbox1.get(tk.ACTIVE)
+        keykey2 = self.listbox2.get(tk.ACTIVE)
         return keykey2
 
     def get_model(self, brand):
@@ -58,11 +64,18 @@ class MainApplication(tk.Frame):
             self.listbox2.insert(tk.END, concentrate)
             del goto[:]
 
-    def get_car_id(self, ):
-        pass
-
     def openfile(self):
         self.loaded_dir = filedialog.askopenfilename()
+
+    def select_model(self, key_listbox):
+        try:
+            self.textbox.delete("1.0", tk.END)
+            pull_num = [int(s) for s in key_listbox.split() if s.isdigit()]
+            self.loaded_id = pull_num[0]
+            self.textbox.insert(tk.END, str(self.loaded_id))
+        except IndexError:
+            self.textbox.delete("1.0", tk.END)
+            self.textbox.insert(tk.END, "Error")
 
 
 if __name__ == "__main__":
